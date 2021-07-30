dfb2bcf11e1dc207b4a55c04388e8a1115ed1fc1
Jessica <jessicaione101@gmail.com>
2021-07-30 16:39:54 -0700

    Done mass and fixed point matrices

--- src/fixed_point_constraints.cpp
+++ src/fixed_point_constraints.cpp
@@ -1,6 +1,11 @@
 #include <fixed_point_constraints.h>
 #include <algorithm>
+
 void fixed_point_constraints(Eigen::SparseMatrixd &P, unsigned int q_size, const std::vector<unsigned int> indices) {
-
-
+  P.resize(3, q_size);
+  for (int i = 0; i < indices.size(); ++i) {
+    P.coeffRef(0, indices[i]) = 1;
+    P.coeffRef(1, indices[i]) = 1;
+    P.coeffRef(2, indices[i]) = 1;
+  }
 }
--- src/mass_matrix_particles.cpp
+++ src/mass_matrix_particles.cpp
@@ -1,6 +1,7 @@
 #include <mass_matrix_particles.h>
 
 void mass_matrix_particles(Eigen::SparseMatrixd &M, Eigen::Ref<const Eigen::VectorXd> q, double mass) {
-
-    
+  M.resize(q.size(), q.size());
+  for (int i = 0, j = 0; i < q.size(); ++i, ++j)
+    M.coeffRef(i, j) = mass;
 }
