9da24c3651c207bb2d9fc387cd5e127e85a0e147
Jessica <jessicaione101@gmail.com>
2021-08-25 17:45:34 -0700

    Done per-spring hessian of the spring potential energy

--- src/d2V_spring_particle_particle_dq2.cpp
+++ src/d2V_spring_particle_particle_dq2.cpp
@@ -1,6 +1,17 @@
 #include <d2V_spring_particle_particle_dq2.h>
 
 void d2V_spring_particle_particle_dq2(Eigen::Ref<Eigen::Matrix66d> H, Eigen::Ref<const Eigen::Vector3d> q0,  Eigen::Ref<const Eigen::Vector3d> q1, double l0, double stiffness) {
+  Eigen::Vector3d delta_x = q1 - q0;
+  double norm = delta_x.norm();
+  double squared_norm = delta_x.squaredNorm();
+  
+  Eigen::Matrix3d term = Eigen::Matrix3d::Constant(stiffness * (norm-l0)/norm);
+  Eigen::Matrix3d quadrant;
+  for (int i = 0; i < delta_x.size(); ++i)
+    for (int j = 0; j < delta_x.size(); ++j)
+      quadrant(i,j) = delta_x(i) * delta_x(j);
+  quadrant = quadrant * (stiffness/squared_norm * ((l0-norm)/norm + 1));
 
-    
+  H.block(0, 0, 3, 3) = H.block(3, 3, 3, 3) = quadrant + term.cwiseProduct(Eigen::Matrix3d::Identity());
+  H.block(3, 0, 3, 3) = H.block(0, 3, 3, 3) = quadrant + term;
 }
