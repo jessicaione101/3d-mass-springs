095f0753c19a60ab4f28525282e956043a96c222
Jessica <jessicaione101@gmail.com>
2021-08-27 17:59:56 -0700

    Fix fixed point matrix

--- src/fixed_point_constraints.cpp
+++ src/fixed_point_constraints.cpp
@@ -2,10 +2,12 @@
 #include <algorithm>
 
 void fixed_point_constraints(Eigen::SparseMatrixd &P, unsigned int q_size, const std::vector<unsigned int> indices) {
-  P.resize(3, q_size);
+  P.resize(3*(q_size - indices.size()), q_size);
+  P.setZero();
   for (int i = 0; i < indices.size(); ++i) {
-    P.coeffRef(0, indices[i]) = 1;
-    P.coeffRef(1, indices[i]) = 1;
-    P.coeffRef(2, indices[i]) = 1;
+    int col = P.cols() - indices[i]*3 - 1;
+    P.coeffRef(i*3,     col)   = 1;
+    P.coeffRef(i*3 + 1, col-1) = 1;
+    P.coeffRef(i*3 + 2, col-2) = 1;
   }
 }
