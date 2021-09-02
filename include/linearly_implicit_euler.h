d8951b187970b48a43ae27abb52039619fb98d47
Jessica <jessicaione101@gmail.com>
2021-09-01 18:52:57 -0700

    Add linearly implicit Euler time integrator

--- include/linearly_implicit_euler.h
+++ include/linearly_implicit_euler.h
@@ -1,6 +1,6 @@
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
-#include<Eigen/SparseCholesky>
+#include <Eigen/SparseCholesky>
 #include <EigenTypes.h>
 
 //Input:
@@ -19,7 +19,23 @@ template<typename FORCE, typename STIFFNESS>
 inline void linearly_implicit_euler(Eigen::VectorXd &q, Eigen::VectorXd &qdot, double dt, 
                             const Eigen::SparseMatrixd &mass,  FORCE &force, STIFFNESS &stiffness, 
                             Eigen::VectorXd &tmp_force, Eigen::SparseMatrixd &tmp_stiffness) {
-    
-
-
+  Eigen::VectorXd f;
+  force(f, q, qdot);
+  Eigen::SparseMatrixd K;
+  stiffness(K, q, qdot);
+  
+  Eigen::SparseMatrixd A = mass - dt*dt*K;
+  Eigen::VectorXd b = mass*qdot + dt*f;
+  Eigen::SimplicialLDLT<Eigen::SparseMatrixd> solver;
+  
+  solver.compute(A);
+  if (solver.info() != Eigen::Success)
+    return;
+  
+  Eigen::VectorXd _qdot = solver.solve(b);
+  if (solver.info() != Eigen::Success)
+    return;
+  
+  qdot = _qdot;
+  q = q + dt*qdot;
 }
