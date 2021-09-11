fbd847730c7207c7e46b8089108e4276a6695506
Jessica <jessicaione101@gmail.com>
2021-09-11 11:32:03 -0700

    Fix stiffness matrix assembly

--- src/assemble_stiffness.cpp
+++ src/assemble_stiffness.cpp
@@ -1,4 +1,5 @@
 #include <assemble_stiffness.h>
+#include <iostream> // TODO remove
 
 void assemble_stiffness(Eigen::SparseMatrixd &K, Eigen::Ref<const Eigen::VectorXd> q, Eigen::Ref<const Eigen::VectorXd> qdot, 
                      Eigen::Ref<const Eigen::MatrixXd> V, Eigen::Ref<const Eigen::MatrixXi> E, Eigen::Ref<const Eigen::VectorXd> l0, 
@@ -21,24 +22,24 @@ void assemble_stiffness(Eigen::SparseMatrixd &K, Eigen::Ref<const Eigen::VectorX
     q1(2) = q(v1*3 + 2);
     
     d2V_spring_particle_particle_dq2(H, q0, q1, l0(i), k);
-    
+
     int row, col;
-    
+
     for (row = 0; row < 3; ++row)
       for (col = 0; col < 3; ++col)
-        coefficients.emplace_back(v0*3 + row, v0*3 + col, -H(row, col));
-    
+        coefficients.emplace_back(v0 * 3 + row, v0 * 3 + col, -H(row, col));
+
     for (row = 0; row < 3; ++row)
       for (col = 3; col < 6; ++col)
-        coefficients.emplace_back(v0*3 + row, v1*3 + col-3, -H(row, col));
-    
+        coefficients.emplace_back(v0 * 3 + row, v1 * 3 + col - 3, -H(row, col));
+
     for (row = 3; row < 6; ++row)
       for (col = 0; col < 3; ++col)
-        coefficients.emplace_back(v1*3 + row-3, v0*3 + col, -H(row, col));
-    
+        coefficients.emplace_back(v1 * 3 + row - 3, v0 * 3 + col, -H(row, col));
+
     for (row = 3; row < 6; ++row)
       for (col = 3; col < 6; ++col)
-        coefficients.emplace_back(v1*3 + row-3, v1*3 + col-3, -H(row, col));
+        coefficients.emplace_back(v1 * 3 + row - 3, v1 * 3 + col - 3, -H(row, col));
   }
   
   K.resize(q.size(), q.size());
