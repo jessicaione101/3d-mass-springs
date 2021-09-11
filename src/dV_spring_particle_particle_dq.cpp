54bcb178302ed683e5e6816bf5b3a17101ca8fb2
Jessica <jessicaione101@gmail.com>
2021-09-11 16:56:44 -0700

    Fix potential energy derivative

--- src/dV_spring_particle_particle_dq.cpp
+++ src/dV_spring_particle_particle_dq.cpp
@@ -3,6 +3,6 @@
 void dV_spring_particle_particle_dq(Eigen::Ref<Eigen::Vector6d> f, Eigen::Ref<const Eigen::Vector3d> q0,  Eigen::Ref<const Eigen::Vector3d> q1, double l0, double stiffness) {
   Eigen::Vector3d delta_x = q1 - q0;
   double norm = delta_x.norm();
-  f << delta_x, q0 - q1;
+  f << -delta_x, delta_x;
   f = f * stiffness * (norm - l0) / norm;
 }
