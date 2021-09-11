6b4944789716434b9a47eae8e0222fe02ab053a0
Jessica <jessicaione101@gmail.com>
2021-09-11 10:55:10 -0700

    Fix hessian

--- src/d2V_spring_particle_particle_dq2.cpp
+++ src/d2V_spring_particle_particle_dq2.cpp
@@ -4,14 +4,16 @@ void d2V_spring_particle_particle_dq2(Eigen::Ref<Eigen::Matrix66d> H, Eigen::Ref
   Eigen::Vector3d delta_x = q1 - q0;
   double norm = delta_x.norm();
   double squared_norm = delta_x.squaredNorm();
-  
-  Eigen::Matrix3d term = Eigen::Matrix3d::Constant(stiffness * (norm-l0)/norm);
+
+  Eigen::Matrix3d term1 = (stiffness * (norm-l0) / norm) * Eigen::Matrix3d::Identity();
+  Eigen::Matrix3d term2 = Eigen::Matrix3d::Constant(stiffness / squared_norm);
+  Eigen::Matrix3d term3 = Eigen::Matrix3d::Constant(stiffness * (norm-l0) / (norm*squared_norm)) ;
+
   Eigen::Matrix3d quadrant;
   for (int i = 0; i < delta_x.size(); ++i)
     for (int j = 0; j < delta_x.size(); ++j)
       quadrant(i,j) = delta_x(i) * delta_x(j);
-  quadrant = quadrant * (stiffness/squared_norm * ((l0-norm)/norm + 1));
 
-  H.block(0, 0, 3, 3) = H.block(3, 3, 3, 3) = quadrant + term.cwiseProduct(Eigen::Matrix3d::Identity());
-  H.block(3, 0, 3, 3) = H.block(0, 3, 3, 3) = quadrant + term;
+  H.block(0, 0, 3, 3) = H.block(3, 3, 3, 3) = quadrant.cwiseProduct(term2) + quadrant.cwiseProduct(-term3) + term1;
+  H.block(3, 0, 3, 3) = H.block(0, 3, 3, 3) = quadrant.cwiseProduct(-term2) + quadrant.cwiseProduct(term3) - term1;
 }
