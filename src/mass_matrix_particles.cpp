2657913a9b1251615ebc6d23be6eb0371a664c31
Jessica <jessicaione101@gmail.com>
2021-09-11 10:50:54 -0700

    Update mass matrix

--- src/mass_matrix_particles.cpp
+++ src/mass_matrix_particles.cpp
@@ -2,6 +2,6 @@
 
 void mass_matrix_particles(Eigen::SparseMatrixd &M, Eigen::Ref<const Eigen::VectorXd> q, double mass) {
   M.resize(q.size(), q.size());
-  for (int i = 0, j = 0; i < q.size(); ++i, ++j)
-    M.coeffRef(i, j) = mass;
+  for (int i = 0; i < q.size(); ++i)
+    M.coeffRef(i, i) = mass;
 }
